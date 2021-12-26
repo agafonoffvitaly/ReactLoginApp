bb7b6c7803c5b81e26ca034a33e9b1250f178e6c
Agafonov <agafonoff.vv@gmail.com>
2021-12-26 23:04:08 +0200

    Dashboard and Preferences. Routes added

--- auth-app/src/components/App/App.js
+++ auth-app/src/components/App/App.js
@@ -1,9 +1,20 @@
-// import './App.css';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Product from '../Product/Product';
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
 
 function App() {
   return (
-    <Product/>
+    <div className="wrapper">
+      <h1>Application</h1>
+      <Router>
+        <Routes>
+          <Route path="/dashboard" component={Dashboard}/>
+          <Route path="/preferences" component={Preferences}/>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
