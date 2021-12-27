6a6b2b01e0293c10ec35d372e68c10e0fb314263
Agafonov <agafonoff.vv@gmail.com>
2021-12-27 17:50:00 +0200

    fake api  and UserManagment added

--- auth-app/src/components/App/App.js
+++ auth-app/src/components/App/App.js
@@ -1,20 +1,28 @@
+import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Product from '../Product/Product';
-import Dashboard from '../Dashboard/Dashboard';
-import Preferences from '../Preferences/Preferences';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from '../Login/Login';
+import UserManagement from '../UserManagement/UserManagement';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
-      <Router>
-        <Routes>
-          <Route path="/dashboard" component={Dashboard}/>
-          <Route path="/preferences" component={Preferences}/>
-        </Routes>
-      </Router>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/users">
+            <UserManagement />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
+
   );
 }
 
